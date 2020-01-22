9f01a9f27c631b815b1be969d8856661d43cf886
anokata <anokata8@mail.ru>
2020-01-22 19:09:40 +0300

    [started] again

---
+++ card21/Main.java
@@ -0,0 +1,16 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Main app = new Main();
+    }
+
+    Main () {
+        System.out.println("Created Main");
+    }
+}
+
