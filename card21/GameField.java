f2a07c14513c99e408d1da014ed827b810889d6e
anokata <anokata8@mail.ru>
2020-01-28 19:17:32 +0300

    draw

--- card21/GameField.java
+++ card21/GameField.java
@@ -15,10 +15,23 @@ class GameField extends JPanel {
     }
 
     public void paintComponent(Graphics g) {
-        g.setColor(Color.white);
-        g.fillRect(0,0,500,250);
-        g.setColor(Color.blue);
-        g.fillRect(10,10,200,250);
+        int deckX = 20;
+        int deckY = 20;
+        int deckWidth = 50;
+        int deckHeigh = 80;
+        int borderWidth = 10;
+        Color textColor = new Color(0,0,0);
+        // draw deck
+        g.setColor(new Color(200, 200, 200));
+        g.fillRoundRect(deckX, deckY, deckWidth, deckHeigh, 20, 20);
+        g.setColor(new Color(150, 0, 0));
+        g.fillRoundRect(deckX+borderWidth/2, deckY+borderWidth/2, deckWidth-borderWidth, deckHeigh-borderWidth, 20, 20);
+
+        // draw card (text)
+        Font f = new Font("DejaVu Mono", Font.BOLD, 12);
+        setFont(f);
+        g.setColor(textColor);
+        g.drawString("A", 0, 0);
     }
 }
 
