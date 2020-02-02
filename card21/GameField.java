71fa6a78913cfa7549a888e81738b2d104ffa303
anokata <anokata8@mail.ru>
2020-02-02 12:47:36 +0300

    [making] draw image

--- card21/GameField.java
+++ card21/GameField.java
@@ -2,6 +2,8 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.awt.Image;
+import java.awt.image.*;
 
 class GameField extends JPanel {
     public static void main(String[] args) {
@@ -20,6 +22,7 @@ class GameField extends JPanel {
         int deckWidth = 50;
         int deckHeigh = 80;
         int borderWidth = 10;
+        Graphics2D g2d = (Graphics2D)g;
         Color textColor = new Color(0,0,0);
         // draw deck
         g.setColor(new Color(200, 200, 200));
@@ -31,7 +34,50 @@ class GameField extends JPanel {
         Font f = new Font("DejaVu Mono", Font.BOLD, 12);
         setFont(f);
         g.setColor(textColor);
-        g.drawString("A", 0, 0);
+        g2d.drawString("A", 100, 0);
+
+        Image image;
+        image = Toolkit.getDefaultToolkit().getImage("cards/CA.png");
+        image = makeColorTransparent(image, Color.BLACK);
+
+        g2d.drawImage(image, 0, 0, this);
+    }
+
+    public static BufferedImage toBufferedImage(Image img) {
+        if (img instanceof BufferedImage) {
+            return (BufferedImage) img;
+        }
+        int width = img.getWidth(null);
+        if (width <= 0) width = 1;
+        int height = img.getHeight(null);
+        if (height <= 0) height = 1;
+        // Create a buffered image with transparency
+        BufferedImage bimage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        // Draw the image on to the buffered image
+        Graphics2D bGr = bimage.createGraphics();
+        bGr.drawImage(img, 0, 0, null);
+        bGr.dispose();
+        // Return the buffered image
+        return bimage;
+    }
+
+    public static Image makeColorTransparent(Image image, final Color color) {
+        BufferedImage im = toBufferedImage(image);
+        ImageFilter filter = new RGBImageFilter() {
+            // the color we are looking for... Alpha bits are set to opaque
+            public int markerRGB = color.getRGB() | 0xFF000000;
+            public final int filterRGB(int x, int y, int rgb) {
+                if ((rgb | 0xFF000000) == markerRGB) {
+                    // Mark the alpha bits as zero - transparent
+                    return 0x00FFFFFF & rgb;
+                } else {
+                    // nothing to do
+                    return rgb;
+                }
+            }
+        };
+        ImageProducer ip = new FilteredImageSource(im.getSource(), filter);
+        return Toolkit.getDefaultToolkit().createImage(ip);
     }
 }
 
